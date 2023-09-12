f5929f69cf73400f7e2005064df99ecfb61b8307
PickledOwen <owend1117@gmail.com>
2023-09-12 02:40:43 -0700

    First rendition of machine learning algorithm

---
+++ nhlpredictor/machine-learning.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn.metrics import precision_score
+from sklearn.ensemble import RandomForestClassifier
+
+matches = pd.read_csv("output.csv", index_col=0, header=0)
+matches["Date"] = pd.to_datetime(matches["Date"])
+matches["Location_code"] = matches["Location"].astype("category").cat.codes
+
+matches = matches.dropna()
+
+test = pd.read_csv("test.csv", index_col=0, header=0)
+test["Date"] = pd.to_datetime(test["Date"])
+test["Location_code"] = test["Location"].astype("category").cat.codes
+
+test = test.dropna()
+
+rf = RandomForestClassifier(n_estimators=200, min_samples_split=100, random_state=1)
+
+
+train = matches
+
+predictors = ["Location_code", "OpponentID", "Point Percentage", "Shots Allowed / Game",
+				"Shots / Game", "Faceoff Win Percentage", "Goals Against / Game", "Goals / Game", "Save Percentage" ]
+
+rf.fit(train[predictors], train["Outcome"])
+
+preds = rf.predict(test[predictors])
+
+error = precision_score(test["Outcome"], preds)
+print(error)
